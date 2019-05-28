6996a67f46b44626fd6a59c2cec2849b3f836cc2
Felipe Martins <felipewmartins@gmail.com>
2019-05-27 22:19:54 -0300

    First commit

---
+++ src/components/loginPanel/index.jsx
@@ -0,0 +1,12 @@
+import {React, Component} from 'react';
+
+const PanelContext = React.createContext('Foo');
+
+export default class LoginPanel extends Component {
+    constructor(this.props) {
+        super(props)
+        this.state = [];
+    }
+
+
+}
